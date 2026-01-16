eca1795f409fe8f76119459e83851c25e780a795
paulalopezv2 <paulalopezvaldes2@gmail.com>
2026-01-15 21:52:52 -0300

    Paso 2 seguridad de datos con Object.freeze

--- paula-lopez/script.js
+++ paula-lopez/script.js
@@ -1,4 +1,4 @@
-console.log("=== PASO 1: REGISTRO DEL HEROE ===");
+console.log("=== PASO 1 ===");
 
 /*objeto aventurero*/
 const aventurero = {
@@ -16,3 +16,24 @@ console.log("Se agrega titulo:", aventurero);
 
 aventurero["puntosDeVida"] = 100;
 console.log("Se agregan puntos de vida:", aventurero);
+
+
+console.log("=== PASO 2 ===");
+
+
+/*objeto de identif */
+const idGremio = {
+  codigo: "GRM-001",
+  fechaRegistro: "2026-01-15"
+};
+
+console.log("ID del Gremio (antes de congelar):", idGremio);
+
+/* Congelar el objeto */
+Object.freeze(idGremio);
+
+/* Intentar modificar el código */
+idGremio.codigo = "GRM-999";
+idGremio.nuevaPropiedad = "Esto no debería existir";
+
+console.log("ID del Gremio (después del intento de modificación):", idGremio);
